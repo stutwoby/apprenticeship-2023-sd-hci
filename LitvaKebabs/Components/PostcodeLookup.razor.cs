d3b637d7f84362cc2562e79db31c010408b43a52
Aidan Fell <Aidan.Fell@hcmercury.onmicrosoft.com>
2023-10-18 10:22:39 +0100

    start work on postcode lookup

--- LitvaKebabs/Components/PostcodeLookup.razor.cs
+++ LitvaKebabs/Components/PostcodeLookup.razor.cs
@@ -1,7 +1,20 @@
-﻿namespace LitvaKebabs.Components
+﻿using Microsoft.AspNetCore.Components;
+
+namespace LitvaKebabs.Components
 {
     public partial class PostcodeLookup
     {
+        private const string deliveringFromPostcode = "BH243FW";
+        [SupplyParameterFromQuery(Name="q")]
+        public string SearchTerm { get; set; }
 
+        protected override async Task OnInitializedAsync()
+        {
+
+        }
+        private decimal CalculateDistance()
+        {
+            return 0;
+        }
     }
 }
