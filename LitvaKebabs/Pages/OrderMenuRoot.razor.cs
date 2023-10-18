5339a09328f8340469112aa66e6ee2a0fd65ca48
Aidan Fell <Aidan.Fell@hcmercury.onmicrosoft.com>
2023-10-18 10:23:03 +0100

    encapsulation encapsulation

--- LitvaKebabs/Pages/OrderMenuRoot.razor.cs
+++ LitvaKebabs/Pages/OrderMenuRoot.razor.cs
@@ -3,33 +3,46 @@ using CsvHelper.Configuration;
 using LitvaKebabs.Models;
 using LitvaKebabs.Components;
 using System.Linq;
+using Microsoft.AspNetCore.Components;
+using System.IO;
 
 namespace LitvaKebabs.Pages
 {
     public partial class OrderMenuRoot
     {
-        private Dictionary<string, decimal> MenuItems()
+        public OrderMenuRoot() 
+        {
+            MenuItems = _menuItems();
+            SauceItems = _sauceItems();
+        }
+
+        [Parameter]
+        public Dictionary<string, decimal> MenuItems { get; set; }
+        [Parameter]
+        public Dictionary<string, decimal> SauceItems { get; set; }
+
+        private Dictionary<string, decimal> _menuItems()
         {
             var config = new CsvConfiguration(System.Globalization.CultureInfo.InvariantCulture)
             {
                 HasHeaderRecord = true,
             };
-            using var reader = new StreamReader("KebabMenu.csv");
+            using var reader = new StreamReader(Environment.CurrentDirectory + @"\csv\\KebabMenu.csv");
             using var csv = new CsvReader(reader, config);
             var records = csv.GetRecords<KebabMenuModel>().ToList();
             Dictionary<string, decimal> menuItems = records.ToDictionary(keySelector: records => records.Item, elementSelector: records => records.Price);
             return menuItems;
         }
-        private Dictionary<string, decimal> SauceItems()
+        private Dictionary<string, decimal> _sauceItems()
         {
             var config = new CsvConfiguration(System.Globalization.CultureInfo.InvariantCulture)
             {
                 HasHeaderRecord = true,
             };
-            using var reader = new StreamReader("SauceMenu.csv");
+            using var reader = new StreamReader(Environment.CurrentDirectory + "csv\\SauceMenu.csv");
             using var csv = new CsvReader(reader, config);
             var records = csv.GetRecords<SauceMenuModel>().ToList();
-            Dictionary<string, decimal> sauceItems = records.ToDictionary(keySelector: records => records.Item, elementSelector: records => records.Price);
+            Dictionary<string, decimal> sauceItems = records.ToDictionary(keySelector: records => records.Sauce, elementSelector: records => records.Price);
             return sauceItems;
         }
     }
