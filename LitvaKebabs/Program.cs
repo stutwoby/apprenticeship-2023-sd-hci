a45c6a89b71ba051ddd98b5b3316098250e3e440
Aidan Fell <Aidan.Fell@hcmercury.onmicrosoft.com>
2023-10-23 09:30:43 +0100

    dont use var, will commit more

--- LitvaKebabs/Program.cs
+++ LitvaKebabs/Program.cs
@@ -2,7 +2,7 @@ using LitvaKebabs;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
+WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
