b956a97074290f9c227368137aac4460b72ffabf
Aidan Fell <Aidan.Fell@hcmercury.onmicrosoft.com>
2023-10-25 15:34:58 +0100

    csv to json

--- LitvaKebabs/Pages/OrderMenuRoot.razor.cs
+++ LitvaKebabs/Pages/OrderMenuRoot.razor.cs
@@ -1,16 +1,12 @@
-﻿using CsvHelper;
-using CsvHelper.Configuration;
-using LitvaKebabs.Models;
-using LitvaKebabs.Components;
-using System.Linq;
+﻿using LitvaKebabs.Models;
 using Microsoft.AspNetCore.Components;
-using System.IO;
+using Newtonsoft.Json;
 
 namespace LitvaKebabs.Pages
 {
     public partial class OrderMenuRoot
     {
-        public OrderMenuRoot() 
+        public OrderMenuRoot()
         {
             MenuItems = _menuItems();
             SauceItems = _sauceItems();
@@ -23,38 +19,18 @@ namespace LitvaKebabs.Pages
 
         private Dictionary<string, decimal> _menuItems()
         {
-            CsvConfiguration config = new CsvConfiguration(System.Globalization.CultureInfo.InvariantCulture)
-            {
-                HasHeaderRecord = true,
-            };
-            try
-            {
-                FileInfo fi = new(@"\KebabMenu.csv");
-                using FileStream fs = fi.Open(FileMode.Open, FileAccess.Read, FileShare.Read);
-                using StreamReader reader = new StreamReader(fs);
-                using CsvReader csv = new CsvReader(reader, config);
-                List<KebabMenuModel> records = csv.GetRecords<KebabMenuModel>().ToList();
-                Dictionary<string, decimal> menuItems = records.ToDictionary(keySelector: records => records.Item, elementSelector: records => records.Price);
-                return menuItems;
-            }
-            catch (FileNotFoundException f)
-            {
-                throw;
-            }
+            string json = File.ReadAllText(@$"{Environment.CurrentDirectory}\KebabMenu.json");
+            List<KebabMenuModel> data = JsonConvert.DeserializeObject<List<KebabMenuModel>>(json);
 
+            Dictionary<string, decimal> menuItems = data.ToDictionary(keySelector: data => data.Item, elementSelector: records => records.Price);
+            return menuItems;
         }
+
         private Dictionary<string, decimal> _sauceItems()
         {
-            CsvConfiguration config = new CsvConfiguration(System.Globalization.CultureInfo.InvariantCulture)
-            {
-                HasHeaderRecord = true,
-            };
-            string saucePath = "SauceMenu.csv";
-            //var testSaucePath = System.IO.Directory.GetFiles("/");
-            using StreamReader reader = new StreamReader(saucePath);
-            using CsvReader csv = new CsvReader(reader, config);
-            List<SauceMenuModel> records = csv.GetRecords<SauceMenuModel>().ToList();
-            Dictionary<string, decimal> sauceItems = records.ToDictionary(keySelector: records => records.Sauce, elementSelector: records => records.Price);
+            string json = File.ReadAllText(@$"{Environment.CurrentDirectory}\SauceMenu.json");
+            List<SauceMenuModel> data = JsonConvert.DeserializeObject<List<SauceMenuModel>>(json);
+            Dictionary<string, decimal> sauceItems = data.ToDictionary(keySelector: records => records.Sauce, elementSelector: records => records.Price);
             return sauceItems;
         }
     }
