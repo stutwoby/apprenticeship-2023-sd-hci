2fc3db7e97510f70261936cb8b0a200d6623d530
Aidan Fell <Aidan.Fell@hcmercury.onmicrosoft.com>
2023-10-25 15:34:17 +0100

    kebab menu model remove csv config

--- LitvaKebabs/Models/KebabMenuModel.cs
+++ LitvaKebabs/Models/KebabMenuModel.cs
@@ -1,13 +1,10 @@
-﻿using CsvHelper.Configuration.Attributes;
-
-namespace LitvaKebabs.Models
+﻿namespace LitvaKebabs.Models
 {
     public class KebabMenuModel
     {
-        [Name("Item")]
+
         public string Item { get; set; } = string.Empty;
 
-        [Name("Price")]
         public decimal Price { get; set; }
     }
 }
