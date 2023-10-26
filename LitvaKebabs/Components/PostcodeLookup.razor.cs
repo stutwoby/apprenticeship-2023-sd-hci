8d21da3012b709ae42c0eb3603d2d11a0d0b92b5
Aidan Fell <Aidan.Fell@hcmercury.onmicrosoft.com>
2023-10-26 09:53:01 +0100

    this was set up to do things in an unexpected way: instead of refreshing ajax style, it would reload the page

--- LitvaKebabs/Components/PostcodeLookup.razor.cs
+++ LitvaKebabs/Components/PostcodeLookup.razor.cs
@@ -12,18 +12,17 @@ namespace LitvaKebabs.Components
         private const string deliveringFromPostcode = "BH243FW";
         private const double deliveringFromPostcodeLon = -1.775614;
         private const double deliveringFromPostcodeLat = 50.84143;
-        private double? deliveringToPostcodeLon;
-        private double? deliveringToPostcodeLat;
-        [SupplyParameterFromQuery(Name = "q")]
+        private double deliveringToPostcodeLon;
+        private double deliveringToPostcodeLat;
         public string DeliveringToPostcode { get; set; } = string.Empty;
         public decimal DeliveryPrice { get; set; }
 
-        protected override async Task OnInitializedAsync()
+        private async Task OnPostcodeLookupButtonClicked()
         {
             // So we firstly need to validate the postcode. Maybe we could utilise the API to do this and be lazy about it?
             // Ok lets do that...
             string deliveringToPostcode = DeliveringToPostcode;
-            RestResponse? postcodeInformation = PostcodeAPIRequest($"api.postcodes.io/postcodes/{deliveringToPostcode}").GetAwaiter().GetResult();
+            RestResponse? postcodeInformation = await PostcodeAPIRequest("https://api.postcodes.io/postcodes/" + deliveringToPostcode);
             PostcodeModel.Root postcodeModel = JsonConvert.DeserializeObject<PostcodeModel.Root>(postcodeInformation.ToString());
             if (postcodeModel != null)
             {
@@ -33,7 +32,7 @@ namespace LitvaKebabs.Components
                         $"failed to get API contents, wrong postcode potentially entered. " +
                         $"Please reload the page and try again.";
                     await jsRuntime.InvokeVoidAsync("alert", not200Error);
-                    logger.LogError(not200Error);
+                    //logger.LogError(not200Error);
                     throw new Exception();
                 }
                 else
@@ -51,7 +50,7 @@ namespace LitvaKebabs.Components
 
         private async Task<RestResponse?> PostcodeAPIRequest(string apiUri)
         {
-            RestClientOptions options = new RestClientOptions(HttpUtility.UrlEncode(apiUri));
+            RestClientOptions options = new RestClientOptions(apiUri);
             RestClient client = new RestClient(options);
             RestRequest request = new RestRequest("");
             request.AddHeader("accept", "application/json");
@@ -65,8 +64,8 @@ namespace LitvaKebabs.Components
         {
             double d1 = deliveringFromPostcodeLat * (Math.PI / 180.0);
             double num1 = deliveringFromPostcodeLon * (Math.PI / 180.0);
-            double d2 = (double)(deliveringToPostcodeLat * (Math.PI / 180.0));
-            double num2 = (double)(deliveringToPostcodeLon * (Math.PI / 180.0) - num1);
+            double d2 = (deliveringToPostcodeLat * (Math.PI / 180.0));
+            double num2 = (deliveringToPostcodeLon * (Math.PI / 180.0) - num1);
             double d3 = Math.Pow(Math.Sin((d2 - d1) / 2.0), 2.0) + Math.Cos(d1) * Math.Cos(d2) * Math.Pow(Math.Sin(num2 / 2.0), 2.0);
             double res = 6376500.0 * (2.0 * Math.Atan2(Math.Sqrt(d3), Math.Sqrt(1.0 - d3)));
             return res * 0.00062137;
