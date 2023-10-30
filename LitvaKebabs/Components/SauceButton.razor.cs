d3f2f87ce8a2576d5884e08e320c7f4842048fbc
Aidan Fell <Aidan.Fell@hcmercury.onmicrosoft.com>
2023-10-30 13:25:50 +0000

    sauce button

---
+++ LitvaKebabs/Components/SauceButton.razor.cs
@@ -0,0 +1,21 @@
+﻿using LitvaKebabs.Pages;
+using Microsoft.AspNetCore.Components;
+
+namespace LitvaKebabs.Components
+{
+    public partial class SauceButton
+    {
+        [Parameter]
+        public Dictionary<string, decimal> tableData { get; set; } = new Dictionary<string, decimal>();
+        OrderMenuRoot orderMenuRoot = new OrderMenuRoot();
+        private void AddMenuItemToReciept()
+        {
+            foreach (var item in orderMenuRoot.MenuItems)
+            {
+                tableData.Add(item.Key, item.Value);
+                StateHasChanged();
+            }
+
+        }
+    }
+}
